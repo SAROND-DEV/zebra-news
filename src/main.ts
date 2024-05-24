658417ce9a5875c8f520ca1567ca298aad0bc292
SAROND <sarond.dev@gmail.com>
2024-05-24 17:46:29 +0300

    fix moment locale

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import moment from 'moment/dist/moment'
-import ru from 'moment/dist/locale/ru'
+import moment from 'moment'
+import 'moment/dist/locale/ru'
 
 const app = createApp(App)
 
@@ -15,4 +15,4 @@ app.use(router)
 
 app.mount('#app')
 
-moment.updateLocale('ru', ru)
+moment.locale('ru')
